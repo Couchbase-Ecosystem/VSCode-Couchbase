0189a6391724037523b2f470ab209d7df9bc51cc
AayushTyagi1 <tyagiaayush5@gmail.com>
2023-02-23 18:52:33 +0530

    DA#122: Modified: Added script in webview to made post request

--- src/webViews/webViewProvider.ts
+++ src/webViews/webViewProvider.ts
@@ -165,8 +165,23 @@ export function getClusterConnectingFormView(): string {
             <input type="password" id="password" name="rollno" placeholder="Password" value="password" required> <br/><br/>
             <label for="connectionIndetifier">Enter Connection Identifier (optional):</label><br>
             <input type="text" id="connectionIdentifier" name="identifier" placeHolder="Connection Identifier" value="" > <br/><br/>
-            <input type="button" value="Submit" onclick="show()"/>
+            <input type="button" value="Submit" onclick="postRequestToConnect()"/>
          </form>
+         <script>
+         function postRequestToConnect(){
+            const vscode = acquireVsCodeApi();
+            let url = document.getElementById('url').value;
+            let username = document.getElementById('username').value;
+            let password = document.getElementById('password').value;
+            let identifier = document.getElementById('connectionIdentifier').value;
+            vscode.postMessage({
+            command: 'submit',
+            url: url,
+            username: username,
+            password: password,
+            connectionIdentifier: identifier
+         })};
+      </script>
       </body>
       </html>
     `;
