88c9463072257aecaed60234189cbb470a70166a
AayushTyagi1 <tyagiaayush5@gmail.com>
2023-03-13 15:37:56 +0530

    DA#122: Added: Format the message for common errors

--- src/notebook/controller.ts
+++ src/notebook/controller.ts
@@ -1,10 +1,14 @@
 import * as vscode from 'vscode';
 import { getActiveConnection } from '../util/connections';
+import {
+    BucketNotFoundError, CollectionNotFoundError, DocumentNotFoundError,
+    ParsingFailureError, ScopeNotFoundError
+} from 'couchbase';
 
 export class QueryKernel {
     private readonly _id = 'couchbase-query-notebook-kernel';
     private readonly _label = 'New Couchbase Query Notebook';
-    private readonly _supportedLanguages = ['json', 'sql++'];
+    private readonly _supportedLanguages = ['sql++', 'json'];
 
     private _executionOrder = 0;
     private readonly _controller: vscode.NotebookController;
@@ -48,8 +52,27 @@ export class QueryKernel {
 
             execution.end(true, Date.now());
         } catch (err) {
+            const errorArray = [];
+            if (
+                err instanceof ParsingFailureError ||
+                err instanceof BucketNotFoundError ||
+                err instanceof CollectionNotFoundError ||
+                err instanceof DocumentNotFoundError ||
+                err instanceof ScopeNotFoundError
+            ) {
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                const { first_error_code, first_error_message, statement } = err.cause as any;
+                errorArray.push({
+                    code: first_error_code,
+                    msg: first_error_message,
+                    query: statement,
+                });
+            }
+            else {
+                errorArray.push(err);
+            }
             execution.replaceOutput([new vscode.NotebookCellOutput([
-                vscode.NotebookCellOutputItem.error(err as Error)
+                vscode.NotebookCellOutputItem.json(errorArray)
             ])]);
         }
         execution.end(false, Date.now());
