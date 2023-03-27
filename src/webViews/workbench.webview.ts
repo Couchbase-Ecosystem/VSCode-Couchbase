893754a4ede6ca47bb2cdb7558c92f686f796dd9
AayushTyagi1 <tyagiaayush5@gmail.com>
2023-03-27 17:55:17 +0530

    DA#126: Modified: Modification in presentaion of message

--- src/webViews/workbench.webview.ts
+++ src/webViews/workbench.webview.ts
@@ -7,23 +7,29 @@ export function getQueryWorkbench(): string {
             <title>Couchbase - New Workbench Feature Coming Soon</title>
             <style>
             body {
-                background-image: linear-gradient(to bottom, white 50%, #c6d8fd 50%);
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-image: linear-gradient(to bottom, white 75%, #FC9C0C 25%);
                 background-repeat: no-repeat;
                 background-attachment: fixed;
-                height: 100%;
                 overflow: hidden;
             }
             .container {
-                margin-top: 60px;
+                margin-top: 180px;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
                 text-align: center;
             }
             h1 {
-                font-size: 48px;
+                font-size: 62px;
                 color: black;
                 margin-bottom: 20px;
             }
             h3 {
-                font-size: 18px;
+                font-size: 32px;
                 color: black;             
             }
             p {
@@ -31,23 +37,14 @@ export function getQueryWorkbench(): string {
                 margin-bottom: 40px;
                 color: black;
             }
-            .coder {
-                border-radius: 50%;
-                box-shadow: 10px 10px 0px #000;
-                height: 250px;
-                margin: 0 auto 60px;
-                width: 290px;
-              }
             </style>
         </head>
         <body>
             <div class="container">
-            <img src="https://www.couchbase.com/wp-content/uploads/2022/08/CB-logo-R_B_B.png" alt="New Workbench Feature Coming Soon"/>
+            <img src="https://www.couchbase.com/wp-content/uploads/2022/08/CB-logo-R_B_B.png" alt="New Workbench Feature Coming Soon"/> <br/><br/>
             <h1>A New Workbench Feature Is Coming Soon</h1>
-            <img class="coder" src="https://raw.githubusercontent.com/couchbaselabs/VSCode-Couchbase/DA%23126-Create-Query-Workbench-Coming-Soon/images/coder.png" alt="Coder At Work"/>
-            <h1>Coders At Work</h1>
             <br/>
-            <h3>Stay Tune</h3>
+            <h3>Stay Tuned!</h3>
             </div>
         </body>
         </html>    
