05eb2e543705fc572484165f11d391539c6db235
AayushTyagi1 <tyagiaayush5@gmail.com>
2023-07-04 20:15:31 +0530

    GG-676: Modified: Fix sharedComponent import problem

--- src/reactViews/app/bootstrap.tsx
+++ src/reactViews/app/bootstrap.tsx
@@ -1,16 +1,24 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import { ButtonVariant } from "sharedComponents/components/button/button.types";
-import { Button } from "sharedComponents/components/button";
+import './index.css';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 const App: React.FC = () => {
-  const submitButton: ButtonVariant = "secondary";
   return (
     <div className="container">
-      <Button variant={submitButton}>Submit Button</Button>
+          <div className="container">
+      <img
+        src="https://www.couchbase.com/wp-content/uploads/2022/08/CB-logo-R_B_B.png"
+        alt="New Workbench Feature Coming Soon"
+      />
+      <br />
+      <br />
+      <h1>A New Workbench Feature Is Coming Soon</h1>
+      <br />
+      <h3>Stay Tuned!</h3>
+    </div>
     </div>
   );
 };
--- src/reactViews/app/index.tsx
+++ src/reactViews/app/index.tsx
@@ -1,23 +1,2 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import './index.css';
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-const App: React.FC = () => {
-  return (
-    <div className="container">
-      <img
-        src="https://www.couchbase.com/wp-content/uploads/2022/08/CB-logo-R_B_B.png"
-        alt="New Workbench Feature Coming Soon"
-      />
-      <br />
-      <br />
-      <h1>A New Workbench Feature Is Coming Soon</h1>
-      <br />
-      <h3>Stay Tuned!</h3>
-    </div>
-  );
-};
-root.render(<App />);
+import ('./bootstrap');
