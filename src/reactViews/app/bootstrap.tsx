f9805a2fe66b92dc2b3481cc9e7140c935869b30
AayushTyagi1 <tyagiaayush5@gmail.com>
2023-07-05 15:16:28 +0530

    GG-677: Added: Query Editor component and Data Table Component

--- src/reactViews/app/bootstrap.tsx
+++ src/reactViews/app/bootstrap.tsx
@@ -1,18 +1,43 @@
 import * as React from "react";
-import { BrowserRouter as Router } from 'react-router-dom';
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import { QueryResults } from "sharedComponents/components/query-results";
-import hotels from '../mocks/query-results/hotel.json';
+import "./index.css";
+import dayjs from "dayjs";
+import { QueryEditor } from "sharedComponents/components/workbench/query-editor";
+import { QueryEditorProps } from "sharedComponents/components/workbench/query-editor/query-editor.types";
+import {
+  QUERY,
+  DATABASE_SCHEMA,
+} from "../mocks/query-editor/query-editor-mocks";
+import { DataTable } from "sharedComponents/components/data-table";
+import hotels from "../mocks/query-results/hotel.json";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+const FALLBACK_MESSAGE = {
+  "No data to display": "Hit 'run' button to run query.",
+};
+
 export const App: React.FC = () => {
+  const queryEditorProps: QueryEditorProps = {
+    query: QUERY,
+    onRun: () => {},
+    queryStatus: "success",
+    lastExecuted: dayjs().toString(),
+    executionTime: "134.123344ms",
+    numDocs: 216,
+    databaseSchema: DATABASE_SCHEMA,
+  };
   return (
-    <div className="h-[300px]">
-      <QueryResults queryResults={hotels} />
-    </div>
+<div>
+  <div className="h-[300px]">
+    <QueryEditor {...queryEditorProps} />
+  </div>
+  <div className="h-[300px]" style={{ marginTop: "200px" }}>
+    <DataTable data={hotels} dataFallback={[FALLBACK_MESSAGE]} />
+  </div>
+</div>
   );
 };
-root.render(<Router><App /></Router>);
+root.render(<App />);
