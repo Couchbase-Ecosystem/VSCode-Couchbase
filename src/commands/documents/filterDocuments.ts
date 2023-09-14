c84e031b94ee0b0ccb2cde294779ae1caff3b5ae
AayushTyagi1 <tyagiaayush5@gmail.com>
2023-09-14 16:32:02 +0530

    DA#220: Added: Implement Document Filter

--- src/commands/documents/filterDocuments.ts
+++ src/commands/documents/filterDocuments.ts
@@ -2,20 +2,19 @@ import CollectionNode from "../../model/CollectionNode";
 import * as vscode from 'vscode';
 import { Memory } from "../../util/util";
 import { IFilterDocuments } from "../../types/IFilterDocuments";
-import ClusterConnectionTreeProvider from "../../tree/ClusterConnectionTreeProvider";
 
-export const filterDocuments = async (node: CollectionNode, clusterConnectionTreeProvider: ClusterConnectionTreeProvider) => {
-    let docFilter = Memory.state.get<IFilterDocuments>(`filterDocuments-${node.connection.connectionIdentifier}-${node.bucketName}-${node.scopeName}-${node.collectionName}`);
-    let filterStmt: string = docFilter ? docFilter.filter : "";
+export const filterDocuments = async (node: CollectionNode) => {
+    const docFilter = Memory.state.get<IFilterDocuments>(`filterDocuments-${node.connection.connectionIdentifier}-${node.bucketName}-${node.scopeName}-${node.collectionName}`);
+    const filterStmt: string = docFilter ? docFilter.filter : "";
     let collectionName = node.collectionName;
     if (collectionName.length > 15) {
         collectionName = collectionName.substring(0, 13) + '...';
     }
-    let newDocFilterStmt = await vscode.window.showInputBox({
-        title: `Apply filter for collection \`${collectionName}\`` ,
-        placeHolder: `airline="AI" OR equipment="772"`,
+    const newDocFilterStmt = await vscode.window.showInputBox({
+        title: `Apply filter for collection \`${collectionName}\``,
+        placeHolder: `airline="AI" OR country="United States"`,
         value: filterStmt,
-        prompt:`SELECT meta.id() FROM \`${collectionName}\` WHERE [Your Filter] |\n (To remove filter, please erase all text)`,
+        prompt: `SELECT meta.id() FROM \`${collectionName}\` WHERE [Your Filter] | `,
         validateInput: (input) => {
             const tokens = input.split(" ");
             for (const token of tokens) {
@@ -29,14 +28,11 @@ export const filterDocuments = async (node: CollectionNode, clusterConnectionTre
     if (newDocFilterStmt === undefined) {
         return;
     }
-    let newDocFilter: IFilterDocuments = {
+    const newDocFilter: IFilterDocuments = {
         filter: newDocFilterStmt.trim()
     };
     Memory.state.update(
         `filterDocuments-${node.connection.connectionIdentifier}-${node.bucketName}-${node.scopeName}-${node.collectionName}`,
         newDocFilter
     );
-
-    // Refresh collections
-    clusterConnectionTreeProvider.refresh(node.parentNode);
 };
