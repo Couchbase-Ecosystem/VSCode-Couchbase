8a765d17e7ff29680a90d4f2a31f6adce6ee95cc
Lokesh Goel <lokesh.goel@couchbase.com>
2023-10-31 10:33:07 +0530

    added tooltips for cli tools

--- src/webViews/tools/dataImport/getKeysAndAdvancedSettings.webview.ts
+++ src/webViews/tools/dataImport/getKeysAndAdvancedSettings.webview.ts
@@ -166,19 +166,48 @@ export const getKeysAndAdvancedSettings = (
                 margin-right:5px;
             }
 
-            
+            .tooltip {
+                position: relative;
+                display: inline-block;
+            }
+
+            .tooltip .tooltiptext {
+                visibility: hidden;
+                width: 400px;
+                background-color: #555;
+                color: #fff;
+                text-align: center;
+                border-radius: 6px;
+                padding: 5px 10px;
+                position: absolute;
+                z-index: 1;
+                bottom: 100%; 
+                left: 50%; 
+                margin-left: -40px; 
+                opacity: 0;
+                transition: opacity 0s;
+            }
+
+            .tooltip:hover .tooltiptext {
+                visibility: visible;
+                opacity: 1;
+            }
 
           </style>
         </head>
         <body>
             <h1 class="heading">Import Data</h1>
+
             <form action="#" method="post" id="dataImportForm">
+            
                 <div class="separator-container">
                     <span class="separator-text">Document Key</span>
                     <div class="separator"></div>
                 </div>
 
-                <label for="keyOptions">Key Options:</label>
+                <label for="keyOptions" class="tooltip">Key Options: 
+                    <span class="tooltiptext">Select how you want to generate the document key for each imported document. You can choose to generate a random UUID, use the value of a field in the data, or generate a custom expression.</span>
+                </label>
                 <select name="keyOptions" id="keyOptions" onchange="onKeyOptionsClick(value)" width="100%">
                     <option value="random" selected>Generate random UUID for each document</option>
                     <option value="fieldValue">Use the value of field as the key</option>
@@ -187,20 +216,34 @@ export const getKeysAndAdvancedSettings = (
                 <br>
 
                 <div id="fieldNameContainer" hidden>
-                    <label for="keyFieldName">Field name:</label>
+                    <label for="keyFieldName" class="tooltip">Field name: 
+                    <span class="tooltiptext">Specify the field in the data that contains the value to use as the document key.</span>
+                </label>
                     <input type="text" name="keyFieldName" id="keyFieldName" value="cbms">
                     <br>
                 </div>
-                <br>
 
                 <div id="customExpressionContainer" hidden>
-                    <label for="customExpression">Custom expression:</label>
+                    <label for="customExpression" class="tooltip">Custom expression: 
+                    <span class="tooltiptext">Specify the custom expression to generate the document key.
+                        You can use the following variables:
+                        <ul style="text-align:left">
+                            <li><b>#UUID#</b> - Random UUID</li>
+                            <li><b>#MONO_INCR#</b> - Document Numbering starting from 1</li>
+                            <li><b>%FIELDNAME%</b> - Value of the field specified above</li>
+                        </ul>
+                    </span>
+                </label>
                     <input type="text" name="customExpression" id="customExpression" value="%cbms%">
                     <br>
                 </div>
-                <br>
+                <div id="UUID-warn">Note: If random UUID or #UUID# tag is used, the UUID values in the preview area might not match the actual UUID values generated during import.</div>
 
-                <label for="keyPreview">Key Preview:</label>
+                <br>
+                
+                <label for="keyPreview" class="tooltip">Key Preview: 
+                    <span class="tooltiptext">Shows the preview of few keys which will be used in document generation. Make sure that keys are different and defined for each document else few documents might fail to import or may get overwritten.</span>
+                </label>
                 <div class="keyPreviewContainer">
                     <div class="getKeyPreviewButton redButton" id="getKeyPreviewButton" onclick="fetchKeyPreview()">Fetch preview</div>
                     <textarea id="keyPreviewTextArea" readonly value="No Preview to show!"> </textarea>
@@ -214,23 +257,33 @@ export const getKeysAndAdvancedSettings = (
                     </div>
                     <div class="advanced-settings" id="advanced-settings">
                         
-                        <label for="skipFirstDocuments">Skip the first # Documents:</label>
+                        <label for="skipFirstDocuments" class="tooltip">Skip the first # Documents: 
+                            <span class="tooltiptext">Specify the number of documents to skip from the beginning of the file.</span>
+                        </label>
                         <input type="number" name="skipFirstDocuments" id="skipFirstDocuments" value="">
                         <br>
 
-                        <label for="importUptoDocuments">Import up to # Documents:</label>
+                        <label for="importUptoDocuments" class="tooltip">Import up to # Documents: 
+                            <span class="tooltiptext">Specify the maximum number of documents to import.</span>
+                        </label>
                         <input type="number" name="importUptoDocuments" id="importUptoDocuments" value="">
                         <br>
                         
-                        <label for="ignoreFields">Ignore the fields:</label>
+                        <label for="ignoreFields" class="tooltip">Ignore the fields: 
+                            <span class="tooltiptext">Specify the fields in the data to ignore.</span>
+                        </label>
                         <input type="text" name="ignoreFields" id="ignoreFields" value="">
                         <br>
 
-                        <label for="threads">Threads</label>
+                        <label for="threads" class="tooltip">Threads 
+                            <span class="tooltiptext">Specify the number of threads to use for importing the data.</span>
+                        </label>
                         <input type="number" name="threads" id="threads" value="4">
                         <br>
                         <div class="verboseLogContainer">
-                            <label for="verboseLog" id="verboseLogLabel">Verbose Log:</label>
+                            <label for="verboseLog" id="verboseLogLabel" class="tooltip">Verbose Log: 
+                            <span class="tooltiptext">Specify whether to enable verbose logging.</span>
+                        </label>
                             <input type="checkbox" name="verboseLog" id="verboseLog">
                         </div>
                     </div>
@@ -249,12 +302,14 @@ export const getKeysAndAdvancedSettings = (
             function onKeyOptionsClick(value) {
                 document.getElementById('fieldNameContainer').setAttribute('hidden',"");
                 document.getElementById('customExpressionContainer').setAttribute('hidden',"");
+                document.getElementById('UUID-warn').setAttribute('hidden',"");
                 if(value === "random") {
-                    // Don't show any field
+                    document.getElementById('UUID-warn').removeAttribute('hidden',"");
                 } else if (value === "fieldValue"){
                     document.getElementById('fieldNameContainer').removeAttribute('hidden');
                 } else if (value === "customExpression") {
                     document.getElementById('customExpressionContainer').removeAttribute('hidden');
+                    document.getElementById('UUID-warn').removeAttribute('hidden',"");
                 }
             }
 
