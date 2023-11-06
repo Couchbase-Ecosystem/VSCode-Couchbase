8ca15367a1dad8b7f2f82adff1361d2f0cc59d98
Aayush Tyagi <tyagiaayush5@gmail.com>
2023-11-06 16:09:29 +0530

    Da#3240couchbase iq support react built (#328)

---
+++ src/reactViews/iq/bootstrap.tsx
@@ -0,0 +1,17 @@
+// @ts-nocheck
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+
+const container = document.getElementById("vscodeRootIQ");
+const newRoot = createRoot(container);
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <p>Hello Couchbase IQ</p>
+    </div>
+  );
+};
+
+newRoot.render(<App />);
