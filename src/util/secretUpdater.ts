161e999e016f34945184e8805a31f9ce50109b0a
Lokesh Goel <lokesh.goel@couchbase.com>
2024-01-31 23:16:09 +0530

    use dotenv to load lambda secrets

--- src/util/secretUpdater.ts
+++ src/util/secretUpdater.ts
@@ -1,7 +1,13 @@
 import { ExtensionContext } from "vscode";
+import * as dotenv from 'dotenv';
 
 // Before Building, Please update secrets else IQ Feedback service will not work
 export const secretUpdater = (context: ExtensionContext) => {
-    context.globalState.update("feedbackLambdaSecret", "c0uchbase_is_aw3some");
-    context.globalState.update("feedbackLambdaUrl", "https://nms548yy5b.execute-api.us-west-1.amazonaws.com/Prod/");
+    dotenv.config();
+    console.log(process.env);
+    console.log("secret: ", process.env.FEEDBACK_LAMBDA_SECRET);
+    console.log("url: ", process.env.FEEDBACK_LAMBDA_URL);
+
+    context.globalState.update("feedbackLambdaSecret", process.env.FEEDBACK_LAMBDA_SECRET);
+    context.globalState.update("feedbackLambdaUrl", process.env.FEEDBACK_LAMBDA_URL);
 };
