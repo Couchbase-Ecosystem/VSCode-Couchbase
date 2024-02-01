93c006d8d5571dfeef386a0cfbf86b5f131711ae
Lokesh Goel <lokesh.goel@couchbase.com>
2024-02-01 10:53:44 +0530

    using dist to store creds

--- src/util/secretUpdater.ts
+++ src/util/secretUpdater.ts
@@ -1,13 +1,12 @@
 import { ExtensionContext } from "vscode";
-import * as dotenv from 'dotenv';
 
-// Before Building, Please update secrets else IQ Feedback service will not work
-export const secretUpdater = (context: ExtensionContext) => {
-    dotenv.config();
-    console.log(process.env);
-    console.log("secret: ", process.env.FEEDBACK_LAMBDA_SECRET);
-    console.log("url: ", process.env.FEEDBACK_LAMBDA_URL);
+import * as fs from 'fs';
+import * as path from 'path';
 
-    context.globalState.update("feedbackLambdaSecret", process.env.FEEDBACK_LAMBDA_SECRET);
-    context.globalState.update("feedbackLambdaUrl", process.env.FEEDBACK_LAMBDA_URL);
+export const secretUpdater = (context: ExtensionContext) => {   
+    const config = JSON.parse(fs.readFileSync(path.join(__filename,"..", "config.json")).toString());
+    console.log("config",config);
+
+    context.globalState.update("feedbackLambdaSecret", config.FEEDBACK_LAMBDA_SECRET);
+    context.globalState.update("feedbackLambdaUrl", config.FEEDBACK_LAMBDA_URL);
 };
