8f1e89bde560d0d71432ef78e953d597f76d9ce4
Lokesh Goel <lokesh.goel@couchbase.com>
2024-02-01 11:52:36 +0530

    remove unwanted logging

--- src/util/secretUpdater.ts
+++ src/util/secretUpdater.ts
@@ -5,7 +5,6 @@ import * as path from 'path';
 
 export const secretUpdater = (context: ExtensionContext) => {   
     const config = JSON.parse(fs.readFileSync(path.join(__filename,"..", "config.json")).toString());
-    console.log("config",config);
 
     context.globalState.update("feedbackLambdaSecret", config.FEEDBACK_LAMBDA_SECRET);
     context.globalState.update("feedbackLambdaUrl", config.FEEDBACK_LAMBDA_URL);
