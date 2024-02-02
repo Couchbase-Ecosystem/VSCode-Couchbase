e536583ccc5e636e6f3c8aac970aa532a0c8fd50
Lokesh Goel <lokesh.goel@couchbase.com>
2024-02-02 16:44:04 +0530

    rollback to copying secrets to dist

--- src/util/secretUpdater.ts
+++ src/util/secretUpdater.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export const secretUpdater = (context: ExtensionContext) => {
-    const config = JSON.parse(fs.readFileSync(path.join(__filename,"..","..","src", "config.json")).toString());
+    const config = JSON.parse(fs.readFileSync(path.join(__filename,"..", "config.json")).toString());
     context.globalState.update("feedbackLambdaSecret", config.FEEDBACK_LAMBDA_SECRET);
     context.globalState.update("feedbackLambdaUrl", config.FEEDBACK_LAMBDA_URL);
 };
