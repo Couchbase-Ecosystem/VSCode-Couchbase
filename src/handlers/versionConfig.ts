a665c797187fbe685ec3868407482ea99ceee5fc
Prajwal Pai <prajwal.pai@couchbase.com>
2024-04-02 12:51:07 +0530

    Add logic to update tools version + keywords for tags

---
+++ src/handlers/versionConfig.ts
@@ -0,0 +1,11 @@
+export const config = {
+    VERSION_FILE: "cb_tool_versions.txt",
+    CBMIGRATE_KEY: "cbmigrate",
+    TOOLS_KEY: "tools",
+    SHELL_KEY: "shell",
+    CBIMPORT_EXPORT_KEY: "cbimport_export",
+    TOOLS_VERSION: "7.2",
+    CBMIGRATE_VERSION: "1",
+    SHELL_VERSION: "1",
+    CBIMPORT_EXPORT_VERSION: "7.2",
+};
