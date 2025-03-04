efd463eaedf8281d5d61163449d5418ca04085f7
AayushTyagi1 <tyagiaayush5@gmail.com>
2025-03-04 14:40:41 +0530

    DA-715 Batch Query Result

--- src/workbench/queryWorkbench.ts
+++ src/workbench/queryWorkbench.ts
@@ -25,6 +25,7 @@ import { getUUID, Memory } from '../util/util';
 import { QueryHistoryTreeProvider } from '../tree/QueryHistoryTreeProvider';
 import { IQueryContext } from '../types/IQueryContext';
 import { getAllNamedParameters } from '../util/namedParameters';
+import { stripComments } from '../util/workbench/stripComments';
 
 export class QueryWorkbench {
     private _untitledSqlppDocumentService: UntitledSqlppDocumentService;
@@ -34,6 +35,98 @@ export class QueryWorkbench {
         this._untitledSqlppDocumentService = new UntitledSqlppDocumentService();
         this.editorToContext = new Map<string, IQueryContext>();
     }
+
+    separateQueries = (batchQuery: string) => {
+        const queries = [];
+        let curQuery = '';
+
+        // AV-90844 - need to remove comments before splitting, but then restore them after
+        const SQL_COMMENT_REGEXP = /--.*$\n?|\/\*[\s\S]*?\*\/|('(?:\\.|[^'\\])*'|"(?:\\.|[^"\\])*")/gm;
+        const comments: string[] = [];
+        let queryNoComments = batchQuery;
+        queryNoComments = queryNoComments.replace(SQL_COMMENT_REGEXP, (match) => {
+            comments.push(match);
+            return '^&&^';
+        });
+
+        // split the query with no comments
+        const findSemicolons = // regex copied from on-prem UI, a parser would be better!
+            /("(?:[^"\\]|\\.)*")|('(?:[^'\\]|\\.)*')|(\/\*(?:.|[\n\r])*\*\/)|(`(?:[^`]|``)*`)|((?:[^;"'`/]|\/(?!\*))+)|(;)/g;
+        let matchArray = findSemicolons.exec(queryNoComments);
+
+        while (matchArray !== null) {
+            curQuery += matchArray[0];
+
+            if (matchArray[0] === ';') {
+                queries.push(curQuery);
+                curQuery = '';
+            }
+
+            matchArray = findSemicolons.exec(queryNoComments);
+        }
+
+        if (curQuery.length > 0) {
+            queries.push(curQuery); // get final query if unterminated with ;
+        }
+
+        // restore comments
+        for (let index = 0; index < queries.length; index++) {
+            while (/\^&&\^/gi.exec(queries[index])) {
+                if (!comments.length) {
+                    break;
+                }
+                queries[index] = queries[index].replace(/\^&&\^/gi, () => {
+                    return comments.shift() || '';
+                });
+            }
+            // if a query has nothing but comments, remove it
+            if (stripComments(queries[index]).trim().length === 0) {
+                queries.splice(index, 1);
+                index--;
+            }
+        }
+
+        return queries;
+    };
+
+    runMultipleQueries = async ({ separatedQueries, queryOptions }: { separatedQueries: string[], queryOptions: QueryOptions }) => {
+        let queryResponses: { batchQuery: string; batchQueryResult: unknown }[] = [];
+        let status = 'success';
+        const connection = getActiveConnection();
+        console.log('separated Queries', separatedQueries);
+
+        for (const separatedQuery of separatedQueries) {
+            console.log('separatedQuery 1', separatedQuery);
+            try {
+                const response = await connection?.cluster?.query(separatedQuery, queryOptions).then(
+                    (response: any) => response,
+                    (error: any) => (typeof error === 'object' && 'response' in error ? error.response : {})
+                );
+
+                const statementType = String(response.controls?.stmtType || '');
+
+
+                queryResponses = [...queryResponses, { batchQuery: separatedQuery, batchQueryResult: response.rows || 'No results' }];
+
+                // stop executing batch queries if one of them failed
+                if (Object.keys(response).length === 0 || response.meta.status !== 'success') {
+                    break;
+                }
+            } catch (error: any) {
+                queryResponses = [
+                    {
+                        batchQuery: separatedQuery,
+                        batchQueryResult: error?.response?.errors || 'Unknown error',
+                    },
+                ];
+                status = 'error';
+                return { rows: queryResponses, busy: false, status };
+            }
+        }
+
+        return { rows: queryResponses, busy: false, status };
+    };
+
 
     runCouchbaseQuery = async (
         workbenchWebviewProvider: WorkbenchWebviewProvider,
@@ -60,7 +153,7 @@ export class QueryWorkbench {
             const queryContext = this.editorToContext.get(activeTextEditor.document.uri.toString());
             const queryContextString = queryContext && (`${queryContext?.bucketName}.${queryContext?.scopeName}`); // Query context string is of format bucketName.ScopeName
             const queryParameters = getAllNamedParameters();
-            
+
             const queryOptions: QueryOptions = {
                 profile: QueryProfileMode.Timings,
                 metrics: true,
@@ -75,19 +168,33 @@ export class QueryWorkbench {
                 await new Promise((resolve) => setTimeout(resolve, 500));
                 await workbenchWebviewProvider.sendQueryResult(JSON.stringify([{ "status": "Executing statement" }]), { queryStatus: QueryStatus.Running }, null);
                 const start = Date.now();
-                const result = await connection.cluster?.query(query, queryOptions);
-                const end = Date.now();
-                const rtt = end - start;
-                const resultSize = result?.meta.metrics?.resultSize;
-                const queryStatusProps = {
-                    queryStatus: result?.meta.status,
-                    rtt: rtt.toString() + " MS",
-                    elapsed: result?.meta.metrics?.elapsedTime.toString() + " MS",
-                    executionTime: result?.meta.metrics?.executionTime + " MS",
-                    numDocs: result?.meta.metrics?.resultCount.toString() + " docs",
-                    size: resultSize ? (resultSize > 1000 ? (resultSize / 1000).toFixed(2) + " KB" : resultSize + " Bytes") : ""
-                };
-                const explainPlan = JSON.stringify(result?.meta.profile?.executionTimings);
+
+                const separatedQueries = this.separateQueries(query);
+                let result;
+                let queryStatusProps = {};
+                let explainPlan = null;
+                let timeWait = 0;
+                if (separatedQueries.length > 1) {
+                    result = await this.runMultipleQueries({ separatedQueries, queryOptions });
+                    queryStatusProps = {
+                        queryStatus: result?.status,
+                    };
+                } else {
+                    result = await connection.cluster?.query(query, queryOptions);
+                    const end = Date.now();
+                    const rtt = end - start;
+                    const resultSize = result?.meta.metrics?.resultSize;
+                    queryStatusProps = {
+                        queryStatus: result?.meta.status,
+                        rtt: rtt.toString() + " MS",
+                        elapsed: result?.meta.metrics?.elapsedTime.toString() + " MS",
+                        executionTime: result?.meta.metrics?.executionTime + " MS",
+                        numDocs: result?.meta.metrics?.resultCount.toString() + " docs",
+                        size: resultSize ? (resultSize > 1000 ? (resultSize / 1000).toFixed(2) + " KB" : resultSize + " Bytes") : ""
+                    };
+                    explainPlan = JSON.stringify(result?.meta.profile?.executionTimings);
+                    timeWait = result?.meta.metrics?.resultSize || 0;
+                }
                 workbenchWebviewProvider.setQueryResult(
                     JSON.stringify(result?.rows),
                     queryStatusProps,
@@ -96,7 +203,6 @@ export class QueryWorkbench {
                 );
                 await saveQuery({ query: query, id: getUUID() });
                 queryHistoryTreeProvider.refresh();
-                let timeWait = result?.meta.metrics?.resultSize || 0;
 
                 await new Promise((resolve) => {
                     setTimeout(resolve, (timeWait / 10000));
@@ -106,13 +212,13 @@ export class QueryWorkbench {
                 if (err instanceof CouchbaseError) {
                     const { first_error_code, first_error_message, statement } =
                         err.cause as any;
-                        if(err.message === "ambiguous timeout") {
-                            errorArray.push({
-                                code: 1080,
-                                msg: `Query Timeout: Timeout ${Memory.state.get('queryTimeout')}s exceeded`,
-                                query: query,
-                            });
-                        } else if (
+                    if (err.message === "ambiguous timeout") {
+                        errorArray.push({
+                            code: 1080,
+                            msg: `Query Timeout: Timeout ${Memory.state.get('queryTimeout')}s exceeded`,
+                            query: query,
+                        });
+                    } else if (
                         first_error_code !== undefined ||
                         first_error_message !== undefined ||
                         statement !== undefined
